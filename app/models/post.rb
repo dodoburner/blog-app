e96c72562daa0969f7778ca29beec007c8776e38
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-19 13:36:45 +0200

    Create update posts counter for a user method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :likes
   has_many :comments
+
+  # def initialize
+  #   update_posts_counter
+  # end
+
+  def update_posts_counter
+    User.find(author_id).update(posts_counter: Post.where(author_id: self.author.id).count)
+  end
 end
