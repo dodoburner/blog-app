e502481d992a41a8aeacad4e1ab67d51fdb336f5
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-21 14:55:54 +0200

    Remove rubocop rule that possibly doesnt allow test to work on other enviroments

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Comment, type: :model do
   user = User.new(name: 'Fred Flinstone', photo: 'https://fake-site/', bio: 'Just a dude')
   post = Post.new(author: user, title: 'Title', text: 'Some Text Bla Bla!!!')
-  subject { Like.new(post:, author: user) }
+  subject { Like.new(post: post, author: user) }
 
   it 'increments comment counter of post' do
     subject.send(:update_likes_counter)
