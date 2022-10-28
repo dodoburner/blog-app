592e77860907a303654e206c617ccd2476741ac9
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-10-28 13:17:55 +0200

    Add create comment funcitionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,20 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def create_comment
+    @comment = Comment.new(params.require(:form_comment).permit(:text))
+    @comment.post = Post.find(params[:id])
+    @comment.author = current_user
+
+    if @comment.save
+      flash[:success] = "Question saved successfully"
+      redirect_to post_path
+    else
+      flash.now[:error] = "Error: Question could not be saved"
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get '/posts/new', to: 'posts#new', as: 'create_post'
   post '/posts/new', to: 'posts#create'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'post'
+  post '/users/:user_id/posts/:id', to: 'posts#create_comment'
 end
