e21d926f08935386b6e4f6b7bf3f2371ae0e8112
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-11 09:23:33 +0100

    Add dependent destroy to User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   after_initialize :init
   after_save :increment_posts_counter
   after_destroy :decrement_posts_counter
