05d87a8a57e71387271f5d7ba8ac775664ea3d96
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-11 17:30:43 +0100

    Fix linters

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -1,16 +1,15 @@
 class Api::AuthenticationController < Api::ApiController
-    skip_before_action :authenticate_token!
-  
-    def create
-      @user = User.find_by_email(params[:email])
-      if @user.valid_password?(params[:password])
-        token = JsonWebToken.encode(user_id: @user.id)
-        time = Time.now + 24.hours.to_i
-        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                       username: @user.name }, status: :ok
-      else
-        render json: { error: 'unauthorized' }, status: :unauthorized
-      end
+  skip_before_action :authenticate_token!
+
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user.valid_password?(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
+                     username: @user.name }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
     end
   end
-  
+end
